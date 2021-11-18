a8e485be5ea3379ec61d2389645e772a69047f1a
B08waffles <brandonkane23167@gmail.com>
2021-11-19 03:16:04 +1000

    books new is workin!

--- views/js/list_users.js
+++ views/js/list_users.js
@@ -1,17 +1,17 @@
-fetch("api/users")
+ fetch("api/users")
     .then(res => res.json())
     .then(users => {
         let userListSection = document.getElementById("user-list")
 
         for (let user of users) {
-            userListSection.innerHTML += `
-            <article class="user">
+            userListSection.innerhtml += `
+            <article class="user"> 
                 <span>${user.userID}</span>
                 <span>${user.username}</span>
                 <span>${user.firstName}</span>
-                <a href="update_user.html?id=${user.userID}">Edit</a>
-                <a href="delete_user.html?id=${user.userID}">Delete</a>
+                <a href="update_user.ejs?id=${user.userID}">Edit</a>
+                <a href="delete_user.ejs?id=${user.userID}">Delete</a> 
             </article>
             `
         }
-    })
+    }) 
